86850611b5d615a2602e5ee7344a9dc145721972
nguye <nguyenv2@outlook.com>
2018-11-05 15:58:44 -0800

    crud operations

--- LoveThemBackAPI/LoveThemBackAPI/Controllers/PetsController.cs
+++ LoveThemBackAPI/LoveThemBackAPI/Controllers/PetsController.cs
@@ -34,20 +34,26 @@ namespace LoveThemBackAPI.Controllers
     [HttpGet]
     public ActionResult<List<Pet>> GetAll()
     {
-      return _context.Pets.ToList();
+      var pet = _context.Pets.ToList();
+      return pet;
     }
 
     [HttpGet("{id}", Name = "GetPet")]
     public ActionResult<Pet> GetById(int id)
     {
       var item = _context.Pets.Find(id);
+      var reviewItem = _context.Reviews.Where(reviews => reviews.PetID == item.PetID).ToList();
       if (item == null)
       {
         return NotFound();
       }
       return item;
     }
-
+    /// <summary>
+    /// adds pet object to json Pet api return
+    /// </summary>
+    /// <param name="Pet">pet object</param>
+    /// <returns></returns>
     [HttpPost]
     public IActionResult Create(Pet Pet)
     {
@@ -56,7 +62,12 @@ namespace LoveThemBackAPI.Controllers
 
       return CreatedAtRoute("GetTodo", new { id = Pet.PetID }, Pet);
     }
-
+    /// <summary>
+    /// updates pet json content in api return
+    /// </summary>
+    /// <param name="id"></param>
+    /// <param name="Pet"></param>
+    /// <returns></returns>
     [HttpPut("{id}")]
     public IActionResult Update(int id, Pet Pet)
     {
@@ -76,7 +87,12 @@ namespace LoveThemBackAPI.Controllers
       _context.SaveChanges();
       return NoContent();
     }
-
+    /// <summary>
+    /// deletes pet content in api return body if right password is given
+    /// </summary>
+    /// <param name="id">pet id to be deleted</param>
+    /// <param name="pass">the password</param>
+    /// <returns></returns>
     [HttpDelete("{id}/{pass}")]
     public IActionResult Delete(int id, int pass)
     {
