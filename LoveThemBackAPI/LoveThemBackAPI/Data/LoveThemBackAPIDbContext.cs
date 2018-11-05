fbfd09c2850d40be9e59d0b8e02568e4a9bf4cc2
nguye <nguyenv2@outlook.com>
2018-11-05 13:46:59 -0800

    api scaffold

---
+++ LoveThemBackAPI/LoveThemBackAPI/Data/LoveThemBackAPIDbContext.cs
@@ -0,0 +1,22 @@
+﻿using LoveThemBackAPI.Models;
+using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace LoveThemBackAPI.Data
+{
+  public class LoveThemBackAPIDbContext : DbContext
+  {
+
+    public LoveThemBackAPIDbContext(DbContextOptions<LoveThemBackAPIDbContext> options) : base(options)
+    {
+
+    }
+
+    public DbSet<Pet> Pets { get; set; }
+    public DbSet<Review> Reviews { get; set; }
+
+  }
+}
