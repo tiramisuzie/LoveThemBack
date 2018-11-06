58f5cf07a68a012652edd0feadd58a00a8da3ad3
Carlos Cadena <cs.cadena@gmail.com>
2018-11-06 11:32:43 -0800

    added reviews model

---
+++ LoveThemBackWebApp/LoveThemBackWebApp/Models/Reviews.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace LoveThemBackWebApp.Models
+{
+    public class Reviews
+    {
+        public string Impression { get; set; }
+        public bool Affectionate { get; set; }
+        public bool Friendly { get; set; }
+        public bool Energy { get; set; }
+        public bool Healthy { get; set; }
+        public bool Intelligent { get; set; }
+        public bool Cheery { get; set; }
+        public bool Playful { get; set; }
+    }
+}
