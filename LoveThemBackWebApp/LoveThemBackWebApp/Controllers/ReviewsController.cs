b374745857a8536a10b4f93c41f08e94c2517370
carlos cadena <carloscadena@live.com>
2018-11-06 11:22:15 -0800

    form for reviews

---
+++ LoveThemBackWebApp/LoveThemBackWebApp/Controllers/ReviewsController.cs
@@ -0,0 +1,16 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace LoveThemBackWebApp.Controllers
+{
+    public class ReviewsController : Controller
+    {
+        public IActionResult Index()
+        {
+            return View();
+        }
+    }
+}
